1f2e727b79de558ad68cdcba37178314e67837f0
mbarto <mbarto@hotmail.com>
2026-03-05 23:49:01 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:48:56.091Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T20:49:00.108Z
+Commit: Fix broken links in documentation
