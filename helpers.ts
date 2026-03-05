0921833cec2a42710ac567d79f914f24e2a28461
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:49 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:47:46.121Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T20:47:48.228Z
+Commit: Fix memory leak in event handlers
