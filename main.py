a77f36661eaf0f34969381f7e1ce4be6a1778ff6
mbarto <mbarto@hotmail.com>
2026-03-05 23:45:21 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:44:32.129Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T20:45:20.145Z
+Commit: Fix memory leak in event handlers
