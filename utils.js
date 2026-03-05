f31fa092cdf3c42d2375957afaf287ff96c3f381
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:42 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:46:37.876Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:47:41.083Z
+Commit: Fix CSS styling bug on mobile devices
