284daaac7b67c1970d04f9f190e002179ed77fbd
mbarto <mbarto@hotmail.com>
2026-03-05 23:50:37 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:49:13.104Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:50:36.148Z
+Commit: Patch security vulnerability in authentication
