477544f9341faa796c6c18cb48320bb23389b86c
mbarto <mbarto@hotmail.com>
2026-03-05 23:48:57 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:48:37.237Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T20:48:56.091Z
+Commit: Resolve timezone issues in date handling
