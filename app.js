24d51d2418cdf1edfbca0abdf220edd67ce60f4d
mbarto <mbarto@hotmail.com>
2026-03-05 23:49:19 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:49:15.303Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T20:49:18.211Z
+Commit: Resolve null pointer exception in user service
