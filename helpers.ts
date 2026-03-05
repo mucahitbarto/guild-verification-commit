05e31e6000cba8f6eea3c629737848278a9f31c9
mbarto <mbarto@hotmail.com>
2026-03-05 23:48:27 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:47:48.228Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:48:26.080Z
+Commit: Fix broken links in documentation
