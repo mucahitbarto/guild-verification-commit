103f63152a81bac734b5478605dd92fcfe48aaaa
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:21 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:47:17.466Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:47:19.962Z
+Commit: Fix CSS styling bug on mobile devices
