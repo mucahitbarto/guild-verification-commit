73f53d0dd79ab53bd20200f94c5fb65ebc305906
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:47 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:47:19.962Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:47:46.121Z
+Commit: Introduce real-time notifications
