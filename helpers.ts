b478e1b604c86ef608205585283a72b9cfcc9e0d
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:18 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:46:36.228Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T20:47:17.466Z
+Commit: Fix memory leak in event handlers
