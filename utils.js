de09133d4c8d95836b5f5c5a5d9a3419555e6826
mbarto <mbarto@hotmail.com>
2026-03-05 23:47:45 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:47:41.083Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:47:44.617Z
+Commit: Patch API endpoint error handling
