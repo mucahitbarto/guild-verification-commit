0065643f78049eb2a972ee43446594a69f5a3da8
mbarto <mbarto@hotmail.com>
2026-03-05 23:44:33 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:43:54.675Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:44:32.129Z
+Commit: Fix broken links in documentation
